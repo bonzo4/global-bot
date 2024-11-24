2b164b590336bc396e1512ec7cb98bda77b2c6ce
hvKalista <alonzo.sabino814@gmail.com>
2024-11-24 12:41:45 -0600

    fix: removed bot manager

--- src/services/api/app.controller.ts
+++ src/services/api/app.controller.ts
@@ -3,11 +3,8 @@ import BotManager from '../discord/manager';
 
 @Controller()
 export class AppController {
-  constructor(private readonly botManager: BotManager) {}
-
   @Get()
   getHello(): string {
-    this.botManager.broadcastAPI();
     return 'Hello World!';
     // return this.appService.getHello();
   }
