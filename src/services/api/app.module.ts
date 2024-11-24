d9ef2b91f1da6e0203ba4a969c5e8d3d59ade43a
hvKalista <alonzo.sabino814@gmail.com>
2024-11-24 12:55:17 -0600

    fix: import HookController

--- src/services/api/app.module.ts
+++ src/services/api/app.module.ts
@@ -2,10 +2,11 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import BroadcastManager from '../discord/broadcastManager';
+import { HookController } from './hook.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [AppController, HookController],
 })
 export class AppModule {
   static registerScheduler(broadcastManager: BroadcastManager): DynamicModule {
